33e2dd8a1324fad5b48db6d73b1577385eb6eb40
Mike Mazur <mmazur@gmail.com>
2014-11-27 07:26:09 -0800

    Retrieve only new events

--- sync_activity.rb
+++ sync_activity.rb
@@ -10,34 +10,40 @@ token = ENV['TRACKER_TOKEN']
 
 connection = Faraday.new(url: 'https://www.pivotaltracker.com')
 
+DB = Sequel.sqlite('db/tracker_events.db')
+
+unless DB.table_exists? :events
+  DB.create_table :events do
+    primary_key :id
+    String :kind
+    DateTime :occurred_at
+    Integer :project_id
+    Integer :project_version
+    Integer :story_id
+    String :from_state
+    String :to_state
+    String :raw_json, text: true
+  end
+end
+
 limit = 1000
 offset = 0
 activities = []
 received_data = true
+max_project_version = DB['SELECT MAX(project_version) FROM events'].first[:'MAX(project_version)']
 while received_data
-  response = connection.get("/services/v5/projects/#{project_id}/activity", limit: limit, offset: offset) do
+  params = { limit: limit, offset: offset }
+  params[:since_version] = max_project_version if max_project_version
+  response = connection.get("/services/v5/projects/#{project_id}/activity", params) do
     |req| req.headers['X-TrackerToken'] = token
   end
   as = JSON.parse response.body
   activities.concat as
   received_data = !as.empty?
-  offset += 400
+  offset += as.length
 end
 
 activities.uniq!
-
-DB = Sequel.sqlite('db/tracker_events.db')
-
-DB.create_table :events do
-  primary_key :id
-  String :kind
-  DateTime :occurred_at
-  Integer :project_id
-  Integer :story_id
-  String :from_state
-  String :to_state
-  String :raw_json, text: true
-end
 
 events = DB[:events]
 
@@ -48,6 +54,7 @@ activities.each do |a|
       kind: a['kind'],
       occurred_at: DateTime.parse(a['occurred_at']),
       project_id: a['project']['id'],
+      project_version: a['project_version'],
       story_id: change['id'],
       from_state: (change['original_values'] || {})['current_state'],
       to_state: (change['new_values'] || {})['current_state'],
